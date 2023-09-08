9e2e2a2ff21f52f1e17b8f3f8ed861836bc0dba6
Yusup Apandi <51903213+yusup-apandi@users.noreply.github.com>
2023-09-08 14:11:33 +0700

    first commit

---
+++ assets/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'dark': '#0A0B0A',
+                'secondary': '#DAE9D8',
+                'primary': '#1A5D11',
+                'gray-muted': '#717172',
+                'light': '#F4F5F4',
+                'active':'rgba(218, 233, 216, 0.20)',
+            },
+        }
+    }
+}
